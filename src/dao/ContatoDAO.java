a088ef405f1cd86232420194aa4d8521a77804ea
ulguim <ulguim@archlinux.localhost.net>
2013-10-08 23:06:38 -0300

    Modificação no método DAO agora esta funcionando

--- src/dao/ContatoDAO.java
+++ src/dao/ContatoDAO.java
@@ -9,6 +9,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +22,6 @@ public class ContatoDAO {
     public ContatoDAO() {
         bd = new ConexaoBanco();
     }
-    
 
     public void inserir(ContatoBEAN contato) throws SQLException, ParseException {
         PreparedStatement stm;
@@ -66,10 +66,51 @@ public class ContatoDAO {
                     cid,
                     prof,
                     grupo);
-            
+
             lista.add(cb);
         }
         bd.close();
         return lista;
+    }
+
+    public List<ContatoBEAN> localizar() throws SQLException {
+        List<ContatoBEAN> lista;
+        lista = new ArrayList<>();
+
+        String sql = "SELECT *,p1.nome as nom_profissao "
+                + "FROM contato as c1 LEFT JOIN profissao as p1 "
+                + "ON c1.id_profissao = p1.id "
+                + "LEFT JOIN  cidade as c2 ON c1.id_cidade = c2.cod_cidade  "
+                + "LEFT JOIN estado as e1 ON c2.cod_estado = e1.cod_estado "
+                + "LEFT JOIN grupo as g1 ON c1.id_grupo = g1.id_grupo ";
+
+        Statement st = bd.conecta().createStatement();
+        System.out.println(sql);
+        ResultSet rs = st.executeQuery(sql);
+        ContatoBEAN cb = null;
+        while (rs.next()) {
+
+            ProfissaoBEAN prof = new ProfissaoBEAN(rs.getInt("id_profissao"), rs.getString("nom_profissao"));
+
+            CidadeBEAN cid = new CidadeBEAN(rs.getInt("cod_cidade"), rs.getInt("cod_estado"), rs.getString("nom_cidade"));
+
+            GrupoBEAN grupo = new GrupoBEAN(rs.getInt("id_grupo"), rs.getString("nom_grupo"));
+
+            cb = new ContatoBEAN(
+                    rs.getString("nome"),
+                    rs.getString("path_img"),
+                    rs.getInt("id"),
+                    rs.getDate("nascimento"),
+                    cid,
+                    prof,
+                    grupo);
+
+            lista.add(cb);
+        }
+
+
+        bd.close();
+        return lista;
+
     }
 }
